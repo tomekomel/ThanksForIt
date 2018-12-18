38e48fd4060eb55d1430e2402b3de2914fd93372
Tomasz Omeljaniuk <tomekomel@gmail.com>
2018-12-18 20:06:28 +0100

    THANKS: add Post stub files, db migrations

--- database/migrations/2018_12_18_190117_create_posts_table.php
+++ database/migrations/2018_12_18_190117_create_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCausesTable extends Migration
+class CreatePostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ class CreateCausesTable extends Migration
      */
     public function up()
     {
-        Schema::create('causes', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id');
-            $table->integer('points');
+            $table->text('title');
             $table->text('body');
             $table->timestamps();
         });
@@ -29,6 +29,6 @@ class CreateCausesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('causes');
+        Schema::dropIfExists('posts');
     }
 }
